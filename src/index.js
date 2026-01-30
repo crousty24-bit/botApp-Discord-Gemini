863903c7cf1e06f4078e4dab34d88d4f1d74625a
crousty24-bit <allen.kch24@gmail.com>
2026-01-30 17:16:35 +0100

    feat: Add import ESM gemini.js & add methods to split answer into chunks
    able to overcome discord limit of < 2k char answers
    
    bot send first part generated text THEN send next parts of the answer IF answer is too long
    
    add several comments to explain this process

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { Client, GatewayIntentBits } from 'discord.js'; //allow to exist on servers, get slash cmd
+import { askGemini } from './gemini.js';
 
 // 1. Création du client Discord
 const client = new Client({
@@ -20,11 +21,24 @@ client.on('interactionCreate', async (interaction) => {
   if (interaction.commandName === 'ask') {
     // 3.3 Récupérer l’option "prompt"
     const prompt = interaction.options.getString('prompt');
-
-    // 3.4 Répondre (test)
-    await interaction.reply({
-      content: `🧪 Reçu : "${prompt}"`,
-    });
+    try {
+      await interaction.deferReply(); // 3.4 Déférer la réponse pour éviter le timeout
+      const answer = await askGemini(prompt); // 3.5 Call Gemini
+      const MAX_LENGTH = 2000;
+      const chunks = []; // 3.6 split answer in parts of < 2000 char
+      for (let i = 0; i < answer.length; i += MAX_LENGTH){
+        chunks.push(answer.slice(i, i + MAX_LENGTH));
+      }
+      await interaction.editReply(chunks[0]); // 3.7 Send first part generated text
+      for (let i = 1; i < chunks.length; i++){
+        await interaction.followUp(chunks[i]); // 3.8 Send next parts generated text
+      }
+    } catch (err) {
+      console.error(err);
+      await interaction.editReply(
+        '❌ Une erreur est survenue lors de la génération de la réponse.'
+      );
+    }
   }
 });
 
